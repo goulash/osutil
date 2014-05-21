622bbb709e148694b5631f60defdea4f68f30a34
Ben Morgan <neembi@gmail.com>
2014-05-21 19:12:31 +0200

    Adding decompression and archive reading functions

---
+++ archive.go
@@ -0,0 +1,104 @@
+// Copyright (c) 2014, Ben Morgan. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package util
+
+import (
+	"archive/tar"
+	"compress/bzip2"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+
+	lzma "github.com/remyoudompheng/go-liblzma"
+)
+
+func ReadFileFromArchive(archive, file string) ([]byte, error) {
+	d, err := NewDecompressor(archive)
+	if err != nil {
+		return nil, err
+	}
+
+	return ReadFileFromTar(d, file)
+}
+
+func ReadFileFromTar(r io.Reader, file string) ([]byte, error) {
+	tr := tar.NewReader(r)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		if hdr.Name == file {
+			bytes, err := ioutil.ReadAll(tr)
+			if err != nil {
+				return nil, err
+			}
+			return bytes, nil
+		}
+	}
+
+	return nil, fmt.Errorf("file '%s' not found", file)
+}
+
+type Decompressor struct {
+	file   os.File
+	reader io.Reader
+	closer io.Closer
+}
+
+func NewDecompressor(filepath string) (io.ReadCloser, error) {
+	var d Decompressor
+	var err error
+
+	d.file, err = os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	switch path.Ext(filepath) {
+	case ".xz":
+		xz, err := lzma.NewReader(d.file)
+		if err != nil {
+			return nil, err
+		}
+		d.reader = xz
+		d.closer = xz
+	case ".gz":
+		gz, err := gzip.NewReader(d.file)
+		if err != nil {
+			return nil, err
+		}
+		d.reader = gz
+		d.closer = gz
+	case ".bz2":
+		d.reader = bzip2.NewReader(d.file)
+	case ".tar":
+		d.reader = d.file
+	default:
+		return nil, fmt.Errorf("unknown file format")
+	}
+
+	return &d, nil
+}
+
+func (d *Decompressor) Read(p []byte) (n int, err error) {
+	return d.reader.Read(p)
+}
+
+func (d *Decompressor) Close() error {
+	if closer != nil {
+		err := closer.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return file.Close()
+}
